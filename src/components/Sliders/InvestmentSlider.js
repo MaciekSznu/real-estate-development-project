6b714d6782835d58a1f6961323169efc6e4db1c5
Maciej Sznurawa <msznurawa@gmail.com>
2020-04-01 17:56:03 +0200

    Add react-awesome-slider

---
+++ src/components/Sliders/InvestmentSlider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
+
+const InvestmentSlider = (
+  <AwesomeSlider cssModule={AwesomeSliderStyles}>
+    <div data-src='../../assets/images/room-4161937_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+    <div data-src='../../assets/images/room-4161938_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+    <div data-src='../../assets/images/room-4161939_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+  </AwesomeSlider>
+);
+
+export default InvestmentSlider;
+
+
+
+
--- src/views/InvestmentView/InvestmentView.js
+++ src/views/InvestmentView/InvestmentView.js
@@ -1,13 +1,28 @@
 import React from "react";
 import styles from "./InvestmentView.module.scss";
+import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 
 const InvestmentView = () => (
+  <>
   <div className={styles.investmentViewWrapper} id='inwestycja'>
     <h1 className={styles.sectionTitle}>Nazwa inwestycji</h1>
     <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
     <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
     <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
   </div>
+  <AwesomeSlider cssModule={AwesomeSliderStyles}>
+    <div data-src='../../assets/images/room-4161937_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+    <div data-src='../../assets/images/room-4161938_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+    <div data-src='../../assets/images/room-4161939_1920.jpg' >
+      <p>Lalalalalla</p>
+    </div>
+  </AwesomeSlider>
+  </>
 );
 
 export default InvestmentView;
