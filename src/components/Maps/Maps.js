15a24799eef92f595b6af52789c9c51660f73983
Maciej Sznurawa <msznurawa@gmail.com>
2020-04-03 09:20:51 +0200

    Add base loation map from react-leaflet

---
+++ src/components/Maps/Maps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import '../../views/Root/index.css';
+
+export const LocationMap = () => {
+  const position = [50.0341625, 19.8990923];
+  return (
+    <Map center={position} zoom={16}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Nasza inwestycja</Popup>
+      </Marker>
+    </Map>
+  );
+};
+
