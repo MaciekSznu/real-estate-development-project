7634e33128802eeb72ff29ee51d8a07edf255c75
Maciej Sznurawa <msznurawa@gmail.com>
2020-04-03 16:23:14 +0200

    Change id placement

--- src/views/InvestmentView/InvestmentView.js
+++ src/views/InvestmentView/InvestmentView.js
@@ -8,8 +8,8 @@ import slide_02 from '../../assets/images/room-4161938_1920.jpg';
 import slide_03 from '../../assets/images/room-4161939_1920.jpg';
 
 const InvestmentView = () => (
-  <div className={styles.investmentViewWrapper}>
-    <div className={styles.investmentViewTextWrapper} id='inwestycja'>
+  <div className={styles.investmentViewWrapper} id='inwestycja'>
+    <div className={styles.investmentViewTextWrapper}>
       <h1 className={styles.sectionTitle}>Nazwa inwestycji</h1>
       <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
       <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
