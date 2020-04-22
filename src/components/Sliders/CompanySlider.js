8df84ccec74ffbf0ba932be7c3707b30feb80cbb
Maciej Sznurawa <msznurawa@gmail.com>
2020-04-22 09:19:18 +0200

    Add CompanySlider, fix some styling issues

---
+++ src/components/Sliders/CompanySlider.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import styles from './CompanySlider.module.scss';
+import './InvestmentSlider.css';
+
+import slide_01 from '../../assets/images/company_01_768_454.jpg';
+import slide_02 from '../../assets/images/company_02_768_458.jpg';
+import slide_03 from '../../assets/images/company_03_768_432.jpg';
+import slide_04 from '../../assets/images/company_04_768_512.jpg';
+import slide_05 from '../../assets/images/company_05_768_576.jpg';
+
+const AutoplayerSlider = withAutoplay(AwesomeSlider);
+
+const CompanySlider = (
+  <AutoplayerSlider bullets={true} play={true}
+  cancelOnInteraction={false}
+  interval={4000} className={styles.sliderWrapper}>
+    <div data-src={slide_01} >
+      <p className={styles.sliderDescription}>2015 - 124 mieszkania</p>
+    </div>
+    <div data-src={slide_02} >
+      <p className={styles.sliderDescription}>2016 - 97 mieszkań</p>
+    </div>
+    <div data-src={slide_03} >
+      <p className={styles.sliderDescription}>2017 - 86 mieszkań</p>
+    </div>
+    <div data-src={slide_04} >
+      <p className={styles.sliderDescription}>2018 - 43 mieszkania</p>
+    </div>
+    <div data-src={slide_05} >
+      <p className={styles.sliderDescription}>2019 - 74 mieszkania</p>
+    </div>
+  </AutoplayerSlider>
+);
+
+export default CompanySlider;
+
+
+
+
--- src/views/CompanyView/CompanyView.js
+++ src/views/CompanyView/CompanyView.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./CompanyView.module.scss";
 import CountersWrapper from './CountersWrapper';
+import CompanySlider from '../../components/Sliders/CompanySlider'
 
 const CompanyView = () => (
   <div className={styles.companyViewWrapper} id='inwestor'>
     <h1 className={styles.sectionTitle}>Inwestor</h1>
-    <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
+    <div className={styles.companyTopSectionWrapper}>
+    <p className={styles.sectionParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est. <br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper vel nibh quis finibus. Donec eu tincidunt ante, auctor porta nulla. Integer semper congue lacus non egestas. Curabitur mollis, lorem in convallis dapibus, enim enim fermentum tortor, et hendrerit risus urna nec metus. Aenean lobortis malesuada tortor. Donec egestas finibus varius. Nulla luctus facilisis malesuada. Suspendisse et ex est.</p>
+    {CompanySlider}
+    </div>
     <CountersWrapper />
   </div>
 );
-
+ 
 export default CompanyView;
